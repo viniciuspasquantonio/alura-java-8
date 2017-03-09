9af746ffa01a6c3a82da398e4fcaa6b6a8288383
Vinicius Pasquantonio <vinicius.pasquantonio@talentfour.com.br>
2017-03-09 14:09:06 -0300

    introducao default methods, forEach e sort

---
+++ src/OrdenaStrings.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class OrdenaStrings {
+	public static void main(String[] args) {
+		List<String> palavras = new ArrayList<>();
+		palavras.add("alura online");
+		palavras.add("casa do código");
+		palavras.add("caelum");
+		Collections.sort(palavras);
+		System.out.println(palavras);
+		Comparator<String> comparador = new ComparadorDeStringPorTamanho();
+//		Collections.sort(palavras, comparador);
+		System.out.println(palavras);
+		palavras.sort(comparador);
+		Consumer<String> consumer = new ImprimeNaLinha();
+		palavras.forEach(consumer);
+	}
+}
+
+
+class ComparadorDeStringPorTamanho implements Comparator<String> {
+    public int compare(String s1, String s2) {
+        if(s1.length() < s2.length()) 
+            return -1;
+        if(s1.length() > s2.length()) 
+            return 1;
+        return 0;
+    }
+}
+
+class ImprimeNaLinha implements Consumer<String>{
+
+	@Override
+	public void accept(String t) {
+		System.out.println(t);
+	}
+	
+}
