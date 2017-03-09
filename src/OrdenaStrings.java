6f278009a648deae7f521bb26a7f2c81b9d90869
Vinicius Pasquantonio <vinicius.pasquantonio@talentfour.com.br>
2017-03-09 14:38:51 -0300

    interfaces funcionais, lambda no lugar de comparator e runnable

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class OrdenaStrings {
 	public static void main(String[] args) {
@@ -10,33 +7,9 @@ public class OrdenaStrings {
 		palavras.add("alura online");
 		palavras.add("casa do código");
 		palavras.add("caelum");
-		Collections.sort(palavras);
-		System.out.println(palavras);
-		Comparator<String> comparador = new ComparadorDeStringPorTamanho();
-//		Collections.sort(palavras, comparador);
-		System.out.println(palavras);
-		palavras.sort(comparador);
-		Consumer<String> consumer = new ImprimeNaLinha();
-		palavras.forEach(consumer);
+		palavras.sort((s1, s2) -> s1.length() - s2.length());
+		palavras.forEach(s -> System.out.println(s));
+
+		new Thread(() -> System.out.println("Executando um Runnable")).start();
 	}
-}
-
-
-class ComparadorDeStringPorTamanho implements Comparator<String> {
-    public int compare(String s1, String s2) {
-        if(s1.length() < s2.length()) 
-            return -1;
-        if(s1.length() > s2.length()) 
-            return 1;
-        return 0;
-    }
-}
-
-class ImprimeNaLinha implements Consumer<String>{
-
-	@Override
-	public void accept(String t) {
-		System.out.println(t);
-	}
-	
 }
