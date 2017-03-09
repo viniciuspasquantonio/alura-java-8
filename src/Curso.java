feb122a1db4f1d7823ffdd49f84568a32f3ccbd2
Vinicius Pasquantonio <vinicius.pasquantonio@talentfour.com.br>
2017-03-09 15:43:56 -0300

    stream, filter, map

---
+++ src/Curso.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Curso {
+	private String nome;
+	private int alunos;
+
+	public Curso(String nome, int alunos) {
+		this.nome = nome;
+		this.alunos = alunos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getAlunos() {
+		return alunos;
+	}
+}
+
---
+++ src/ExemploCurso.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ExemploCurso {
+	public static void main(String[] args) {
+		List<Curso> cursos = new ArrayList<Curso>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+		
+		cursos.sort(Comparator.comparing(Curso::getAlunos));
+		cursos.stream()
+				.filter(c->c.getAlunos()>50)
+				.map(c->c.getNome());
+				
+		cursos.stream()
+		   .filter(c -> c.getAlunos() > 50)
+		   .map(Curso::getAlunos)
+		   .forEach(System.out::println);
+	}
+}
